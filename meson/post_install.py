c6f2ff0f13fa609054d00f489679fda256d064ab
Elias Aebi <eyelash@users.noreply.github.com>
2018-03-23 14:29:11 +0100

    post_install: honor the prefix

--- meson/post_install.py
+++ meson/post_install.py
@@ -3,14 +3,14 @@
 import os
 import subprocess
 
-schemadir = os.path.join(os.environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+install_prefix = os.environ['MESON_INSTALL_PREFIX']
+schemadir = os.path.join(install_prefix, 'share', 'glib-2.0', 'schemas')
 
 if not os.environ.get('DESTDIR'):
 	print('Compiling gsettings schemas...')
 	subprocess.call(['glib-compile-schemas', schemadir])
 	print('Renaming icons...')
-	os.rename('/usr/share/icons/hicolor/16x16/mimetypes/com.github.alecaddd.akira.svg', '/usr/share/icons/hicolor/16x16/mimetypes/application-x-akira.svg')
-	os.rename('/usr/share/icons/hicolor/24x24/mimetypes/com.github.alecaddd.akira.svg', '/usr/share/icons/hicolor/24x24/mimetypes/application-x-akira.svg')
-	os.rename('/usr/share/icons/hicolor/32x32/mimetypes/com.github.alecaddd.akira.svg', '/usr/share/icons/hicolor/32x32/mimetypes/application-x-akira.svg')
-	os.rename('/usr/share/icons/hicolor/64x64/mimetypes/com.github.alecaddd.akira.svg', '/usr/share/icons/hicolor/64x64/mimetypes/application-x-akira.svg')
-	os.rename('/usr/share/icons/hicolor/128x128/mimetypes/com.github.alecaddd.akira.svg', '/usr/share/icons/hicolor/128x128/mimetypes/application-x-akira.svg')
+	for size in ['16x16', '24x24', '32x32', '64x64', '128x128']:
+		src = os.path.join(install_prefix, 'share', 'icons', 'hicolor', size, 'mimetypes', 'com.github.alecaddd.akira.svg')
+		dst = os.path.join(install_prefix, 'share', 'icons', 'hicolor', size, 'mimetypes', 'application-x-akira.svg')
+		os.rename(src, dst)
