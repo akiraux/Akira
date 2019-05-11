akiraux/Akira

commit b3d99d930c5cbbe0cf20db25f13f81d82ec79ba1

Settings: reduce code duplication (#158)
* SettingsWindow: don't pass main window reference

* Settings: avoid duplicated code by binding buttons to settings

We use a bunch of functions to display/hide the labels and change the image type.
This creates a new IconButton object that listen to settings signals and change those depending on the state.

* Partials: Create a ButtonImage Object

A lot of duplicate parts of code is due to the symbolic/non-symbolic setting,=
Having an object that handles the changes allows us to use images pretty easily

* SettingsDialog: don't set the window as a parent of settings

As the settings window is a modal window now, it's just need the transient for property.

* Flatpak: add dconf access

Otherwise settings are not usable at all.

* ZoomButton: bind labels visibility to gsettings changes

* AlignBoxButton: display alignement btn images

src/Partials/HeaderBarButton.vala
@@ -20,59 +20,33 @@
 */
 
 public class Akira.Partials.HeaderBarButton : Gtk.Grid {
-    public bool labeled {
-        get {
-            return label_btn.visible;
-        } set {
-            label_btn.visible = value;
-            label_btn.no_show_all = !value;
-        }
-    }
 
-    private Gtk.Label label_btn;
     public Gtk.Button button;
-    public Gtk.Image image;
+    private Gtk.Label label_btn;
+    public Akira.Partials.ButtonImage image;
 
     public HeaderBarButton (string icon_name, string name, string[]? accels = null) {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon_name)) : icon_name;
-
-        image = new Gtk.Image.from_icon_name (icon, size);
-        image.margin = 0;
+        settings.changed["show-label"].connect( () => {
+            label_btn.visible = settings.show_label;
+            label_btn.no_show_all = !settings.show_label;
+        });
 
         button = new Gtk.Button ();
+
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
-        button.add (image);
         button.tooltip_markup = Granite.markup_accel_tooltip (accels, name);
+
+        image = new ButtonImage (icon_name);
+        button.add (image);
 
         attach (button, 0, 0, 1, 1);
         attach (label_btn, 0, 1, 1, 1);
+
         valign = Gtk.Align.CENTER;
-    }
-
-    public void toggle () {
-        labeled = !labeled;
-    }
-
-    public void show_labels () {
-        labeled = true;
-    }
-
-    public void hide_labels () {
-        labeled = false;
-    }
-
-    public void update_image () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (image.icon_name)) : image.icon_name.replace ("-symbolic", "");
-        button.remove (image);
-        image = new Gtk.Image.from_icon_name (new_icon, size);
-        button.add (image);
-        image.show_all ();
     }
 }

src/Partials/MenuButton.vala
@@ -20,60 +20,33 @@
 */
 
 public class Akira.Partials.MenuButton : Gtk.Grid {
-    public bool labeled {
-        get {
-            return label_btn.visible;
-        } set {
-            label_btn.visible = value;
-            label_btn.no_show_all = !value;
-        }
-    }
 
-    private Gtk.Label label_btn;
     public Gtk.MenuButton button;
-    public Gtk.Image image;
+    private Gtk.Label label_btn;
+    public Akira.Partials.ButtonImage image;
 
     public MenuButton (string icon_name, string name, string[]? accels = null) {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon_name)) : icon_name;
+        settings.changed["show-label"].connect( () => {
+            label_btn.visible = settings.show_label;
+            label_btn.no_show_all = !settings.show_label;
+        });
 
-        image = new Gtk.Image.from_icon_name (icon, size);
-        image.margin = 0;
 
-        button = new Gtk.MenuButton ();
+        button = new Gtk.MenuButton();
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
-        button.add (image);
         button.tooltip_markup = Granite.markup_accel_tooltip (accels, name);
+
+        image = new ButtonImage (icon_name);
+        button.add (image);
 
         attach (button, 0, 0, 1, 1);
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
-    }
-
-    public void toggle () {
-        labeled = !labeled;
-    }
-
-    public void show_labels () {
-        labeled = true;
-    }
-
-    public void hide_labels () {
-        labeled = false;
-    }
-
-    public void update_image () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (image.icon_name)) : image.icon_name.replace ("-symbolic", "");
-        button.remove (image);
-        image = new Gtk.Image.from_icon_name (new_icon, size);
-        button.add (image);
-        image.show_all ();
     }
 }

src/Partials/ZoomButton.vala
@@ -22,15 +22,6 @@
 public class Akira.Partials.ZoomButton : Gtk.Grid {
     public weak Akira.Window window { get; construct; }
 
-    public bool labeled {
-        get {
-            return label_btn.visible;
-        } set {
-            label_btn.visible = value;
-            label_btn.no_show_all = !value;
-        }
-    }
-
     private Gtk.Label label_btn;
     public Gtk.Button zoom_out_button;
     public Gtk.Button zoom_default_button;
@@ -78,19 +69,12 @@ public class Akira.Partials.ZoomButton : Gtk.Grid {
         label_btn.get_style_context ().add_class ("headerbar-label");
         label_btn.margin_top = 4;
 
+        settings.changed["show-label"].connect( () => {
+            label_btn.visible = settings.show_label;
+            label_btn.no_show_all = !settings.show_label;
+        });
+
         attach (label_btn, 0, 1, 3, 1);
-    }
-
-    public void toggle () {
-        labeled = !labeled;
-    }
-
-    public void show_labels () {
-        labeled = true;
-    }
-
-    public void hide_labels () {
-        labeled = false;
     }
 
     public void zoom_out () {

src/Layouts/HeaderBar.vala
@@ -172,23 +172,6 @@ public class Akira.Layouts.HeaderBar : Gtk.HeaderBar {
         // dinamically toggle button sensitivity based on document status or actor selected.
     }
 
-    public void update_icons_style () {
-        menu.update_image ();
-        toolset.update_image ();
-        export.update_image ();
-        preferences.update_image ();
-        group.update_image ();
-        ungroup.update_image ();
-        move_up.update_image ();
-        move_down.update_image ();
-        move_top.update_image ();
-        move_bottom.update_image ();
-        path_difference.update_image ();
-        path_exclusion.update_image ();
-        path_intersect.update_image ();
-        path_union.update_image ();
-    }
-
     public void toggle () {
         toggled = !toggled;
     }

src/Partials/AlignBoxButton.vala
@@ -25,7 +25,7 @@ public class Akira.Partials.AlignBoxButton : Gtk.Button {
 
     public string icon { get; construct; }
     public string action { get; construct; }
-    public Gtk.Image btn_image;
+    public Akira.Partials.ButtonImage btn_image;
 
     public AlignBoxButton (Akira.Window main_window, string action_name, string icon_name, string tooltip, string[] accels) {
         Object (
@@ -42,30 +42,13 @@ public class Akira.Partials.AlignBoxButton : Gtk.Button {
         get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
         sensitive = false;
 
-        update_icon_style ();
+        btn_image = new ButtonImage (icon);
+        add (btn_image);
         connect_signals ();
     }
 
-    private void update_icon_style () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon)) : icon.replace ("-symbolic", "");
-
-        if (btn_image != null) {
-            remove (btn_image);
-        }
-
-        btn_image = new Gtk.Image.from_icon_name (new_icon, size);
-        add (btn_image);
-
-        show_all ();
-    }
 
     private void connect_signals () {
-        window.event_bus.update_icons_style.connect (() => {
-            update_icon_style ();
-        });
-
         clicked.connect (() => {
             window.event_bus.emit ("align-items", action);
         });

src/Partials/ButtonImage.vala
@@ -1,0 +1,46 @@
+/*
+* Copyright (c) 2019 Alecaddd (http://alecaddd.com)
+*
+* This file is part of Akira.
+*
+* Akira is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Akira is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with Akira.  If not, see <https://www.gnu.org/licenses/>.
+*
+* Authored by: Bilal Elmoussaoui <bilal.elmoussaoui@gnome.org>
+*/
+
+
+public class Akira.Partials.ButtonImage: Gtk.Image {
+
+    private string icon;
+
+    public ButtonImage (string icon_name) {
+        icon = icon_name;
+        margin = 0;
+
+        settings.changed["use-symbolic"].connect(() => {
+            update_image();
+        });
+
+        update_image ();
+    }
+
+
+    private void update_image () {
+        var size = settings.use_symbolic ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
+        var icon = settings.use_symbolic ? ("%s-symbolic".printf (icon)) : icon.replace ("-symbolic", "");
+        set_from_icon_name(icon, size);
+    }
+
+
+}

src/Services/ActionManager.vala
@@ -35,7 +35,6 @@ public class Akira.Services.ActionManager : Object {
     public const string ACTION_PRESENTATION = "action_presentation";
     public const string ACTION_PREFERENCES = "action_preferences";
     public const string ACTION_EXPORT = "action_export";
-    public const string ACTION_LABELS = "action_labels";
     public const string ACTION_QUIT = "action_quit";
     public const string ACTION_ZOOM_IN = "action_zoom_in";
     public const string ACTION_ZOOM_OUT = "action_zoom_out";
@@ -56,7 +55,6 @@ public class Akira.Services.ActionManager : Object {
         { ACTION_PRESENTATION, action_presentation },
         { ACTION_PREFERENCES, action_preferences },
         { ACTION_EXPORT, action_export },
-        { ACTION_LABELS, action_labels },
         { ACTION_QUIT, action_quit },
         { ACTION_ZOOM_IN, action_zoom_in },
         { ACTION_ZOOM_OUT, action_zoom_out },
@@ -99,73 +97,6 @@ public class Akira.Services.ActionManager : Object {
         }
     }
 
-    private void action_labels () {
-        window.headerbar.toggle ();
-        window.headerbar.menu.toggle ();
-        window.headerbar.toolset.toggle ();
-        window.headerbar.zoom.toggle ();
-        window.headerbar.preferences.toggle ();
-        window.headerbar.export.toggle ();
-        window.headerbar.group.toggle ();
-        window.headerbar.ungroup.toggle ();
-        window.headerbar.move_up.toggle ();
-        window.headerbar.move_down.toggle ();
-        window.headerbar.move_top.toggle ();
-        window.headerbar.move_bottom.toggle ();
-        window.headerbar.path_difference.toggle ();
-        window.headerbar.path_exclusion.toggle ();
-        window.headerbar.path_intersect.toggle ();
-        window.headerbar.path_union.toggle ();
-        window.headerbar.toggle ();
-    }
-
-    public void show_labels () {
-        window.headerbar.toggle ();
-        window.headerbar.menu.show_labels ();
-        window.headerbar.toolset.show_labels ();
-        window.headerbar.zoom.show_labels ();
-        window.headerbar.preferences.show_labels ();
-        window.headerbar.export.show_labels ();
-        window.headerbar.group.show_labels ();
-        window.headerbar.ungroup.show_labels ();
-        window.headerbar.move_up.show_labels ();
-        window.headerbar.move_down.show_labels ();
-        window.headerbar.move_top.show_labels ();
-        window.headerbar.move_bottom.show_labels ();
-        window.headerbar.path_difference.show_labels ();
-        window.headerbar.path_exclusion.show_labels ();
-        window.headerbar.path_intersect.show_labels ();
-        window.headerbar.path_union.show_labels ();
-        window.headerbar.toggle ();
-    }
-
-    public void hide_labels () {
-        window.headerbar.toggle ();
-        window.headerbar.menu.hide_labels ();
-        window.headerbar.toolset.hide_labels ();
-        window.headerbar.zoom.hide_labels ();
-        window.headerbar.preferences.hide_labels ();
-        window.headerbar.export.hide_labels ();
-        window.headerbar.group.hide_labels ();
-        window.headerbar.ungroup.hide_labels ();
-        window.headerbar.move_up.hide_labels ();
-        window.headerbar.move_down.hide_labels ();
-        window.headerbar.move_top.hide_labels ();
-        window.headerbar.move_bottom.hide_labels ();
-        window.headerbar.path_difference.hide_labels ();
-        window.headerbar.path_exclusion.hide_labels ();
-        window.headerbar.path_intersect.hide_labels ();
-        window.headerbar.path_union.hide_labels ();
-        window.headerbar.toggle ();
-    }
-
-    public void update_icons_style () {
-        window.headerbar.toggle ();
-        window.headerbar.update_icons_style ();
-        window.headerbar.toggle ();
-
-        window.event_bus.emit ("update-icons-style");
-    }
 
     private void action_quit () {
         window.before_destroy ();
@@ -203,16 +134,10 @@ public class Akira.Services.ActionManager : Object {
     }
 
     private void action_preferences () {
-        if (window.settings_dialog == null) {
-            window.settings_dialog = new Akira.Widgets.SettingsDialog (window);
-            window.settings_dialog.show_all ();
-
-            window.settings_dialog.destroy.connect (() => {
-                window.settings_dialog = null;
-            });
-        }
-
-        window.settings_dialog.present ();
+        var settings_dialog = new Akira.Widgets.SettingsDialog();
+        settings_dialog.transient_for = window;
+        settings_dialog.show_all ();
+        settings_dialog.present ();
     }
 
     private void action_export () {

src/Widgets/SettingsDialog.vala
@@ -20,7 +20,6 @@
 */
 
 public class Akira.Widgets.SettingsDialog : Gtk.Dialog {
-    public weak Akira.Window window { get; construct; }
 
     private Gtk.Stack main_stack;
     private Gtk.Switch dark_theme_switch;
@@ -31,14 +30,13 @@ public class Akira.Widgets.SettingsDialog : Gtk.Dialog {
         ICONTYPE
     }
 
-    public SettingsDialog (Akira.Window parent) {
+    public SettingsDialog () {
         Object (
             border_width: 5,
             deletable: false,
             resizable: false,
-            title: _("Preferences"),
-            transient_for: parent,
-            window: parent
+            modal: true,
+            title: _("Preferences")
         );
     }
 
@@ -93,6 +91,7 @@ public class Akira.Widgets.SettingsDialog : Gtk.Dialog {
 
         content_grid.attach (new SettingsLabel (_("Use Dark Theme:")), 0, 1, 1, 1);
         dark_theme_switch = new SettingsSwitch ("dark-theme");
+
         content_grid.attach (dark_theme_switch, 1, 1, 1, 1);
 
         dark_theme_switch.notify["active"].connect (() => {
@@ -105,22 +104,11 @@ public class Akira.Widgets.SettingsDialog : Gtk.Dialog {
         label_switch = new SettingsSwitch ("show-label");
         content_grid.attach (label_switch, 1, 3, 1, 1);
 
-        label_switch.notify["active"].connect (() => {
-            if (!settings.show_label) {
-                window.action_manager.hide_labels ();
-            } else if (settings.show_label) {
-                window.action_manager.show_labels ();
-            }
-        });
-
         content_grid.attach (new SettingsLabel (_("Use Symbolic Icons:")), 0, 4, 1, 1);
         symbolic_switch = new SettingsSwitch ("use-symbolic");
         content_grid.attach (symbolic_switch, 1, 4, 1, 1);
 
-        symbolic_switch.notify["active"].connect (() => {
-            window.action_manager.update_icons_style ();
-        });
-
+        
         return content_grid;
     }
 

commit 1131779b25691fc98189515021cde61ea0c2e3e6

Fixed update label on creation. Regressed by #158

src/Partials/HeaderBarButton.vala
@@ -29,13 +29,7 @@ public class Akira.Partials.HeaderBarButton : Gtk.Grid {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
         button = new Gtk.Button ();
-
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
@@ -48,5 +42,15 @@ public class Akira.Partials.HeaderBarButton : Gtk.Grid {
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 }

src/Partials/MenuButton.vala
@@ -29,12 +29,6 @@ public class Akira.Partials.MenuButton : Gtk.Grid {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
-
         button = new Gtk.MenuButton();
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
@@ -48,5 +42,15 @@ public class Akira.Partials.MenuButton : Gtk.Grid {
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 }

src/Partials/ZoomButton.vala
@@ -69,12 +69,17 @@ public class Akira.Partials.ZoomButton : Gtk.Grid {
         label_btn.get_style_context ().add_class ("headerbar-label");
         label_btn.margin_top = 4;
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
         attach (label_btn, 0, 1, 3, 1);
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 
     public void zoom_out () {