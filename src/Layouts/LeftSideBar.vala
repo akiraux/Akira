akiraux/Akira

commit b3d99d930c5cbbe0cf20db25f13f81d82ec79ba1

Settings: reduce code duplication (#158)
* SettingsWindow: don't pass main window reference

* Settings: avoid duplicated code by binding buttons to settings

We use a bunch of functions to display/hide the labels and change the image type.
This creates a new IconButton object that listen to settings signals and change those depending on the state.

* Partials: Create a ButtonImage Object

A lot of duplicate parts of code is due to the symbolic/non-symbolic setting,=
Having an object that handles the changes allows us to use images pretty easily

* SettingsDialog: don't set the window as a parent of settings

As the settings window is a modal window now, it's just need the transient for property.

* Flatpak: add dconf access

Otherwise settings are not usable at all.

* ZoomButton: bind labels visibility to gsettings changes

* AlignBoxButton: display alignement btn images

src/Partials/AlignBoxButton.vala
@@ -25,7 +25,7 @@ public class Akira.Partials.AlignBoxButton : Gtk.Button {
 
     public string icon { get; construct; }
     public string action { get; construct; }
-    public Gtk.Image btn_image;
+    public Akira.Partials.ButtonImage btn_image;
 
     public AlignBoxButton (Akira.Window main_window, string action_name, string icon_name, string tooltip, string[] accels) {
         Object (
@@ -42,30 +42,13 @@ public class Akira.Partials.AlignBoxButton : Gtk.Button {
         get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
         sensitive = false;
 
-        update_icon_style ();
+        btn_image = new ButtonImage (icon);
+        add (btn_image);
         connect_signals ();
     }
 
-    private void update_icon_style () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon)) : icon.replace ("-symbolic", "");
-
-        if (btn_image != null) {
-            remove (btn_image);
-        }
-
-        btn_image = new Gtk.Image.from_icon_name (new_icon, size);
-        add (btn_image);
-
-        show_all ();
-    }
 
     private void connect_signals () {
-        window.event_bus.update_icons_style.connect (() => {
-            update_icon_style ();
-        });
-
         clicked.connect (() => {
             window.event_bus.emit ("align-items", action);
         });

src/Partials/ButtonImage.vala
@@ -1,0 +1,46 @@
+/*
+* Copyright (c) 2019 Alecaddd (http://alecaddd.com)
+*
+* This file is part of Akira.
+*
+* Akira is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* Akira is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with Akira.  If not, see <https://www.gnu.org/licenses/>.
+*
+* Authored by: Bilal Elmoussaoui <bilal.elmoussaoui@gnome.org>
+*/
+
+
+public class Akira.Partials.ButtonImage: Gtk.Image {
+
+    private string icon;
+
+    public ButtonImage (string icon_name) {
+        icon = icon_name;
+        margin = 0;
+
+        settings.changed["use-symbolic"].connect(() => {
+            update_image();
+        });
+
+        update_image ();
+    }
+
+
+    private void update_image () {
+        var size = settings.use_symbolic ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
+        var icon = settings.use_symbolic ? ("%s-symbolic".printf (icon)) : icon.replace ("-symbolic", "");
+        set_from_icon_name(icon, size);
+    }
+
+
+}

src/Partials/HeaderBarButton.vala
@@ -20,59 +20,33 @@
 */
 
 public class Akira.Partials.HeaderBarButton : Gtk.Grid {
-    public bool labeled {
-        get {
-            return label_btn.visible;
-        } set {
-            label_btn.visible = value;
-            label_btn.no_show_all = !value;
-        }
-    }
 
-    private Gtk.Label label_btn;
     public Gtk.Button button;
-    public Gtk.Image image;
+    private Gtk.Label label_btn;
+    public Akira.Partials.ButtonImage image;
 
     public HeaderBarButton (string icon_name, string name, string[]? accels = null) {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon_name)) : icon_name;
-
-        image = new Gtk.Image.from_icon_name (icon, size);
-        image.margin = 0;
+        settings.changed["show-label"].connect( () => {
+            label_btn.visible = settings.show_label;
+            label_btn.no_show_all = !settings.show_label;
+        });
 
         button = new Gtk.Button ();
+
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
-        button.add (image);
         button.tooltip_markup = Granite.markup_accel_tooltip (accels, name);
+
+        image = new ButtonImage (icon_name);
+        button.add (image);
 
         attach (button, 0, 0, 1, 1);
         attach (label_btn, 0, 1, 1, 1);
+
         valign = Gtk.Align.CENTER;
-    }
-
-    public void toggle () {
-        labeled = !labeled;
-    }
-
-    public void show_labels () {
-        labeled = true;
-    }
-
-    public void hide_labels () {
-        labeled = false;
-    }
-
-    public void update_image () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (image.icon_name)) : image.icon_name.replace ("-symbolic", "");
-        button.remove (image);
-        image = new Gtk.Image.from_icon_name (new_icon, size);
-        button.add (image);
-        image.show_all ();
     }
 }

src/Partials/MenuButton.vala
@@ -20,60 +20,33 @@
 */
 
 public class Akira.Partials.MenuButton : Gtk.Grid {
-    public bool labeled {
-        get {
-            return label_btn.visible;
-        } set {
-            label_btn.visible = value;
-            label_btn.no_show_all = !value;
-        }
-    }
 
-    private Gtk.Label label_btn;
     public Gtk.MenuButton button;
-    public Gtk.Image image;
+    private Gtk.Label label_btn;
+    public Akira.Partials.ButtonImage image;
 
     public MenuButton (string icon_name, string name, string[]? accels = null) {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var icon = settings.use_symbolic == true ? ("%s-symbolic".printf (icon_name)) : icon_name;
+        settings.changed["show-label"].connect( () => {
+            label_btn.visible = settings.show_label;
+            label_btn.no_show_all = !settings.show_label;
+        });
 
-        image = new Gtk.Image.from_icon_name (icon, size);
-        image.margin = 0;
 
-        button = new Gtk.MenuButton ();
+        button = new Gtk.MenuButton();
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
-        button.add (image);
         button.tooltip_markup = Granite.markup_accel_tooltip (accels, name);
+
+        image = new ButtonImage (icon_name);
+        button.add (image);
 
         attach (button, 0, 0, 1, 1);
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
-    }
-
-    public void toggle () {
-        labeled = !labeled;
-    }
-
-    public void show_labels () {
-        labeled = true;
-    }
-
-    public void hide_labels () {
-        labeled = false;
-    }
-
-    public void update_image () {
-        var size = settings.use_symbolic == true ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
-        var new_icon = settings.use_symbolic == true ? ("%s-symbolic".printf (image.icon_name)) : image.icon_name.replace ("-symbolic", "");
-        button.remove (image);
-        image = new Gtk.Image.from_icon_name (new_icon, size);
-        button.add (image);
-        image.show_all ();
     }
 }

commit 8fb6824ec0f5f4e3097e47496238a4dd99f655cd

House keeping and bug fixes (#160)
* Style updates, fixed resizing issues for left panel

* Remove status bar, fixed height gsettings change

* Fixed update label on creation. Regressed by #158

* Fix hover effect line width when zooming

* Update UI based on gsettings at the right time. Fixes #136

* Small CSS update

data/css/layout.css
@@ -1,9 +1,3 @@
-.statusbar {
-    background-color: shade (@bg_color, 0.9);
-    color: shade (@fg_color, 0.7);
-    border-top: 1px solid shade (@bg_color, 0.8);
-}
-
 .sidebar-l {
     background-color: shade (@bg_color, 0.95);
     border-right: 1px solid shade (@bg_color, 0.9);
@@ -19,7 +13,7 @@
 }
 
 .fills-box-panel {
-    padding: 3pt 5pt;
+    padding: 3px 5px;
     background-color: @base_color;
     border-bottom: 1px solid shade (@bg_color, 0.7);
 }
@@ -30,36 +24,36 @@
 }
 
 .fills-box-panel button {
-    padding: 4pt;
+    padding: 4px;
     border-radius: 100%;
 }
 
 .fills-list {
     background-color: shade (@bg_color, 0.9);
     border-bottom: 1px solid shade (@bg_color, 0.7);
-    padding: 8pt;
-    padding-bottom: 12pt;
+    padding: 8px;
+    padding-bottom: 12px;
 }
 
 .fill-chooser-cont {
-    margin-bottom: 8pt;
+    margin-bottom: 8px;
 }
 
 .fill-chooser-cont button {
     padding: 0;
-    margin-left: 6pt;
-    border-radius: 100%;
+    margin-left: 6px;
+    border-radius: 50%;
 }
 
 .fill-chooser {
     background-color: shade (@bg_color, 0.98);
     border: 1px solid shade (@bg_color, 0.7);
-    border-radius: 2pt;
+    border-radius: 2px;
 }
 
 .fill-chooser button.selected-color {
-    margin: 5pt;
-    padding: 6pt 10pt;
+    margin: 5px;
+    padding: 6px 10px;
     border-radius: 0;
     border: 1px solid;
     transition: none;
@@ -67,33 +61,27 @@
 
 .fill-chooser label.opacity {
     border-left: 1px solid shade (@bg_color, 0.7);
-    padding: 0 8pt;
+    padding: 0 8px;
     background-color: shade (@base_color, 1);
-    border-top-right-radius: 2pt;
-    border-bottom-right-radius: 2pt;
+    border-top-right-radius: 2px;
+    border-bottom-right-radius: 2px;
 }
 
 .opacity-slider {
-  padding: 3pt 6pt;
+  padding: 3px 6px;
 }
 
-.padding {
-  padding: 8pt 5pt;
+.color-picker {
+  padding: 8px 5px;
 }
 
 .popover-toggler {
-    padding: 3pt;
-    margin: 3pt;
-}
-
-.flat-btn {
-    background: none;
-    margin: 0;
-    padding: 0;
+    padding: 3px;
+    margin: 3px;
 }
 
 .popover-list row {
-    padding: 3pt;
+    padding: 3px;
 }
 
 .popover-list row:selected,
@@ -109,19 +97,17 @@
 .alignment-box {
     background-color: @base_color;
     border-bottom: 1px solid shade (@bg_color, 0.8);
-    padding: 3pt 5pt;
-}
-
-.alignment-box button {
-    border-radius: 100%;
-    padding: 3pt;
-    margin: 0;
+    padding: 3px;
 }
 
 .alignment-box separator {
-    margin: 4pt 3pt;
-    border-right-color: @bg_color;
-    border-right-width: 1px;
+    border-right: none;
+}
+
+.button-rounded {
+    border-radius: 50%;
+    padding-left: 3px;
+    padding-right: 3px;
 }
 
 .group-title {
@@ -159,10 +145,6 @@
 
 .linked .reversed {
     border-right: 0;
-}
-
-.flip-button {
-    padding: 0;
 }
 
 .panel-separator {

data/schemas/com.github.akiraux.akira.gschema.xml.in
@@ -13,21 +13,21 @@
         </key>
 
         <key name="window-width" type="i">
-            <default>1000</default>
+            <default>1024</default>
             <summary>The saved width of the window.</summary>
-            <description>The saved width of the window. Must be greater than 1000, or it will not take effect.</description>
+            <description>The saved width of the window.</description>
         </key>
 
         <key name="window-height" type="i">
-            <default>800</default>
+            <default>768</default>
             <summary>The saved height of the window.</summary>
-            <description>The saved height of the window. Must be greater than 800, or it will not take effect.</description>
+            <description>The saved height of the window.</description>
         </key>
 
         <key name="right-paned" type="i">
-            <default>2000</default>
+            <default>804</default>
             <summary>The saved width of the right panel.</summary>
-            <description>The saved width of the right panel containing the MainCanvas and RightSidebar. Starts at 2000 to push have a decently sized layout.</description>
+            <description>The saved width of the right panel containing the MainCanvas and RightSidebar.</description>
         </key>
 
         <key name="left-paned" type="i">

src/Layouts/LeftSideBar.vala
@@ -41,11 +41,10 @@ public class Akira.Layouts.LeftSideBar : Gtk.Grid {
 
     construct {
         get_style_context ().add_class ("sidebar-l");
-        width_request = 220;
 
         var align_items_panel = new Akira.Layouts.Partials.AlignItemsPanel (window);
-        var fill_box_panel = new Akira.Layouts.Partials.FillsBoxPanel (window);
         var transorm_panel = new Akira.Layouts.Partials.TransformPanel ();
+        var fill_box_panel = new Akira.Layouts.Partials.FillsBoxPanel (window);
 
         attach (align_items_panel, 0, 0, 1, 1);
         attach (transorm_panel, 0, 1, 1, 1);

src/Layouts/MainWindow.vala
@@ -25,9 +25,7 @@ public class Akira.Layouts.MainWindow : Gtk.Grid {
     public Akira.Layouts.MainCanvas main_canvas;
     public Akira.Layouts.LeftSideBar left_sidebar;
     public Akira.Layouts.RightSideBar right_sidebar;
-    public Akira.Layouts.StatusBar statusbar;
 
-    public Gtk.Grid grid;
     public Gtk.Paned pane;
     public Gtk.Paned pane2;
 
@@ -38,23 +36,16 @@ public class Akira.Layouts.MainWindow : Gtk.Grid {
     construct {
         left_sidebar = new Akira.Layouts.LeftSideBar (window);
         right_sidebar = new Akira.Layouts.RightSideBar (window);
-        statusbar = new Akira.Layouts.StatusBar ();
         main_canvas = new Akira.Layouts.MainCanvas ();
 
-        grid = new Gtk.Grid ();
-        pane = new Gtk.Paned (Gtk.Orientation.HORIZONTAL);
         pane2 = new Gtk.Paned (Gtk.Orientation.HORIZONTAL);
-        pane.position = settings.left_paned;
-        pane2.position = settings.right_paned;
-        grid.attach (pane2, 0, 0, 1, 1);
-
-        pane.pack1 (left_sidebar, false, false);
-        pane.pack2 (grid, true, false);
-
         pane2.pack1 (main_canvas, true, false);
         pane2.pack2 (right_sidebar, false, false);
 
+        pane = new Gtk.Paned (Gtk.Orientation.HORIZONTAL);
+        pane.pack1 (left_sidebar, false, false);
+        pane.pack2 (pane2, true, false);
+
         attach (pane, 0, 0, 1, 1);
-        attach (statusbar, 0, 1, 1, 1);
     }
 }

src/Layouts/Partials/AlignItemsPanel.vala
@@ -21,7 +21,7 @@
 public class Akira.Layouts.Partials.AlignItemsPanel : Gtk.Grid {
     public weak Akira.Window window { get; construct; }
 
-    public int current_button_column { get; set; default = 0;}
+    public int current_button_column { get; set; default = 0; }
 
     private struct AlignBoxItem {
         public string type;
@@ -61,21 +61,22 @@ public class Akira.Layouts.Partials.AlignItemsPanel : Gtk.Grid {
         Object (
             window: window,
             orientation: Gtk.Orientation.VERTICAL
-            );
+        );
     }
 
     construct {
-        halign = Gtk.Align.FILL;
-        column_homogeneous = true;
         get_style_context ().add_class ("alignment-box");
+        column_homogeneous = true;
+        hexpand = true;
 
         foreach (var item in ALIGN_ITEMS_PANEL_BUTTONS) {
             switch (item.type) {
                 case "sep":
                     var separator = new Gtk.Separator (Gtk.Orientation.VERTICAL);
                     separator.halign = Gtk.Align.CENTER;
+                    separator.margin_top = separator.margin_bottom = 4;
 
-                    attach (separator, current_button_column++, 0, 1, 1 );
+                    attach (separator, current_button_column++, 0, 1, 1);
                     break;
 
                 case "btn":

src/Layouts/Partials/FillItem.vala
@@ -138,8 +138,7 @@ public class Akira.Layouts.Partials.FillItem : Gtk.Grid {
     selected_blending_mode.halign = Gtk.Align.START;
 
     selected_blending_mode_cont = new Gtk.Button ();
-    selected_blending_mode_cont.get_style_context ().add_class ("flat");
-    selected_blending_mode_cont.get_style_context ().add_class ("flat-btn");
+    selected_blending_mode_cont.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
     selected_blending_mode_cont.can_focus = false;
     selected_blending_mode_cont.hexpand = true;
     selected_blending_mode_cont.add (selected_blending_mode);
@@ -196,7 +195,7 @@ public class Akira.Layouts.Partials.FillItem : Gtk.Grid {
     color_chooser_widget.show_editor = true;
 
     color_picker = new Gtk.Grid ();
-    color_picker.get_style_context ().add_class ("padding");
+    color_picker.get_style_context ().add_class ("color-picker");
     color_picker.attach (color_chooser_widget, 0, 0, 1, 1);
 
     var popover_item_index = 0;

src/Layouts/Partials/TransformPanel.vala
@@ -50,8 +50,9 @@ public class Akira.Layouts.Partials.TransformPanel : Gtk.Grid {
         
         var lock_changes = new Gtk.Button.from_icon_name ("changes-allow-symbolic");
         lock_changes.can_focus = false;
-        lock_changes.tooltip_text = _("Keep Ratio");
-        lock_changes.get_style_context ().add_class ("flat");
+        lock_changes.tooltip_text = _("Lock Ratio");
+        lock_changes.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
+        lock_changes.get_style_context ().add_class ("button-rounded");
         lock_changes.get_style_context ().add_class ("label-colors");
         bind_property (
             "size-lock", lock_changes, "image", BindingFlags.SYNC_CREATE,
@@ -68,18 +69,18 @@ public class Akira.Layouts.Partials.TransformPanel : Gtk.Grid {
         var rotation = new Akira.Partials.LinkedInput (C_("The first letter of Rotation", "R"), "°");
         rotation.unit = "°";
 
-        var hflip_button = new Gtk.Button.from_icon_name ("object-flip-horizontal", Gtk.IconSize.DND);
-        hflip_button.get_style_context ().add_class ("flat");
-        hflip_button.get_style_context ().add_class ("flip-button");
+        var hflip_button = new Gtk.Button.from_icon_name ("object-flip-horizontal", Gtk.IconSize.LARGE_TOOLBAR);
+        hflip_button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
+        hflip_button.get_style_context ().add_class ("button-rounded");
         hflip_button.hexpand = false;
         hflip_button.halign = Gtk.Align.CENTER;
         hflip_button.valign = Gtk.Align.CENTER;
         hflip_button.can_focus = false;
         hflip_button.tooltip_markup = Granite.markup_accel_tooltip ({"<Ctrl><Shift>bracketleft"}, _("Flip Horizontally"));
 
-        var vflip_button = new Gtk.Button.from_icon_name ("object-flip-vertical", Gtk.IconSize.DND);
-        vflip_button.get_style_context ().add_class ("flat");
-        vflip_button.get_style_context ().add_class ("flip-button");
+        var vflip_button = new Gtk.Button.from_icon_name ("object-flip-vertical", Gtk.IconSize.LARGE_TOOLBAR);
+        vflip_button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
+        vflip_button.get_style_context ().add_class ("button-rounded");
         vflip_button.hexpand = false;
         vflip_button.halign = Gtk.Align.CENTER;
         vflip_button.valign = Gtk.Align.CENTER;

src/Layouts/StatusBar.vala
@@ -1,49 +1,0 @@
-/*
-* Copyright (c) 2019 Alecaddd (http://alecaddd.com)
-*
-* This file is part of Akira.
-*
-* Akira is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-
-* Akira is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-
-* You should have received a copy of the GNU General Public License
-* along with Akira.  If not, see <https://www.gnu.org/licenses/>.
-*
-* Authored by: Alessandro "Alecaddd" Castellani <castellani.ale@gmail.com>
-*/
-
-public class Akira.Layouts.StatusBar : Gtk.Grid {
-    public bool toggled {
-        get {
-            return visible;
-        } set {
-            visible = value;
-            no_show_all = !value;
-        }
-    }
-
-    public StatusBar () {
-        Object (toggled: true);
-    }
-
-    construct {
-        get_style_context ().add_class ("statusbar");
-
-        var label = new Gtk.Label ("Status Bar");
-        label.halign = Gtk.Align.CENTER;
-        label.margin = 6;
-
-        attach (label, 0, 0, 1, 1);
-    }
-
-    public void toggle () {
-        toggled = !toggled;
-    }
-}

src/Lib/Canvas.vala
@@ -417,7 +417,6 @@ public class Akira.Lib.Canvas : Goo.Canvas {
 
         var item = (target as Goo.CanvasItemSimple);
         var stroke = item.line_width / 2;
-        var line_width = 1.0 / current_scale;
         var real_width = width + stroke * 2;
         var real_height = height + stroke * 2;
 
@@ -472,7 +471,7 @@ public class Akira.Lib.Canvas : Goo.Canvas {
 
         var item = (target as Goo.CanvasItemSimple);
 
-        var line_width = get_scale () * 2;
+        var line_width = 2.0 / get_scale ();
         var stroke = item.line_width;
         var real_x = x - stroke;
         var real_y = y - stroke;

src/Partials/AlignBoxButton.vala
@@ -37,16 +37,17 @@ public class Akira.Partials.AlignBoxButton : Gtk.Button {
     }
 
     construct {
-        can_focus = false;
-        halign = Gtk.Align.CENTER;
         get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
+        get_style_context ().add_class ("button-rounded");
+        halign = Gtk.Align.CENTER;
+        valign = Gtk.Align.CENTER;
+        can_focus = false;
         sensitive = false;
 
-        btn_image = new ButtonImage (icon);
+        btn_image = new ButtonImage (icon, Gtk.IconSize.SMALL_TOOLBAR);
         add (btn_image);
         connect_signals ();
     }
-
 
     private void connect_signals () {
         clicked.connect (() => {

src/Partials/ButtonImage.vala
@@ -22,9 +22,11 @@
 public class Akira.Partials.ButtonImage: Gtk.Image {
 
     private string icon;
+    private Gtk.IconSize size;
 
-    public ButtonImage (string icon_name) {
+    public ButtonImage (string icon_name, Gtk.IconSize icon_size = Gtk.IconSize.LARGE_TOOLBAR) {
         icon = icon_name;
+        size = icon_size;
         margin = 0;
 
         settings.changed["use-symbolic"].connect(() => {
@@ -35,8 +37,8 @@ public class Akira.Partials.ButtonImage: Gtk.Image {
     }
 
     private void update_image () {
-        var size = settings.use_symbolic ? Gtk.IconSize.SMALL_TOOLBAR : Gtk.IconSize.LARGE_TOOLBAR;
+        var size = settings.use_symbolic ? Gtk.IconSize.SMALL_TOOLBAR : size;
         var icon = settings.use_symbolic ? ("%s-symbolic".printf (icon)) : icon.replace ("-symbolic", "");
-        set_from_icon_name(icon, size);
+        set_from_icon_name (icon, size);
     }
 }

src/Partials/HeaderBarButton.vala
@@ -29,13 +29,7 @@ public class Akira.Partials.HeaderBarButton : Gtk.Grid {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
         button = new Gtk.Button ();
-
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
         button.get_style_context ().add_class (Gtk.STYLE_CLASS_FLAT);
@@ -48,5 +42,15 @@ public class Akira.Partials.HeaderBarButton : Gtk.Grid {
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 }

src/Partials/MenuButton.vala
@@ -29,12 +29,6 @@ public class Akira.Partials.MenuButton : Gtk.Grid {
         label_btn = new Gtk.Label (name);
         label_btn.get_style_context ().add_class ("headerbar-label");
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
-
         button = new Gtk.MenuButton();
         button.can_focus = false;
         button.halign = Gtk.Align.CENTER;
@@ -48,5 +42,15 @@ public class Akira.Partials.MenuButton : Gtk.Grid {
         attach (label_btn, 0, 1, 1, 1);
 
         valign = Gtk.Align.CENTER;
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 }

src/Partials/ZoomButton.vala
@@ -69,12 +69,17 @@ public class Akira.Partials.ZoomButton : Gtk.Grid {
         label_btn.get_style_context ().add_class ("headerbar-label");
         label_btn.margin_top = 4;
 
-        settings.changed["show-label"].connect( () => {
-            label_btn.visible = settings.show_label;
-            label_btn.no_show_all = !settings.show_label;
-        });
-
         attach (label_btn, 0, 1, 3, 1);
+        udpate_label ();
+
+        settings.changed["show-label"].connect( () => {
+            udpate_label ();
+        });
+    }
+
+    private void udpate_label () {
+        label_btn.visible = settings.show_label;
+        label_btn.no_show_all = !settings.show_label;
     }
 
     public void zoom_out () {

src/Services/ActionManager.vala
@@ -103,7 +103,6 @@ public class Akira.Services.ActionManager : Object {
 
     private void action_presentation () {
         window.headerbar.toggle ();
-        window.main_window.statusbar.toggle ();
         window.main_window.left_sidebar.toggle ();
         window.main_window.right_sidebar.toggle ();
     }

src/Services/Settings.vala
@@ -22,10 +22,10 @@
 public class Akira.Services.Settings : Granite.Services.Settings {
     public int pos_x { get; set; }
     public int pos_y { get; set; }
-    public int window_width { get; set; default = 1000; }
-    public int window_height { get; set; default = 600; }
-    public int right_paned { get; set; default = 2000; }
-    public int left_paned { get; set; default = 220; }
+    public int window_width { get; set; }
+    public int window_height { get; set; }
+    public int right_paned { get; set; }
+    public int left_paned { get; set; }
     public bool dark_theme { get; set; }
     public bool show_label { get; set; }
     public bool use_symbolic { get; set; }

src/Window.vala
@@ -58,7 +58,6 @@ public class Akira.Window : Gtk.ApplicationWindow {
         build_ui ();
 
         move (settings.pos_x, settings.pos_y);
-        resize (settings.window_width, settings.window_height);
 
         show_app ();
     }
@@ -86,6 +85,10 @@ public class Akira.Window : Gtk.ApplicationWindow {
         Gtk.StyleContext.add_provider_for_screen (
             Gdk.Screen.get_default (), css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         );
+
+        resize (settings.window_width, settings.window_height);
+        main_window.pane.position = settings.left_paned;
+        main_window.pane2.position = settings.right_paned;
     }
 
     public bool before_destroy () {
@@ -128,14 +131,13 @@ public class Akira.Window : Gtk.ApplicationWindow {
         settings.pos_y = y;
         settings.window_width = width;
         settings.window_height = height;
-        settings.right_paned = main_window.main_canvas.get_allocated_width ();
-        settings.left_paned = main_window.left_sidebar.get_allocated_width ();
+        settings.left_paned = main_window.pane.get_position ();
+        settings.right_paned = main_window.pane2.get_position ();
     }
 
     public void show_app () {
-        show_all ();
         apply_user_settings ();
-
+        show_all ();
         show ();
         present ();
     }

src/meson.build
@@ -39,7 +39,6 @@ executable(
     'Layouts/MainCanvas.vala',
     'Layouts/MainWindow.vala',
     'Layouts/RightSideBar.vala',
-    'Layouts/StatusBar.vala',
     'Layouts/Partials/LayersPanel.vala',
     'Layouts/Partials/PagesPanel.vala',
     'Layouts/Partials/Artboard.vala',