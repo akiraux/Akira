7c5fc6ee4783bd035bcde9704ee8be8d17d5ce33
Alecaddd <castellani.ale@gmail.com>
2024-09-14 19:54:58 -0700

    Add gtk deps and kick-off boilerplate window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &str = "com.github.akiraux.akira";
+
+fn main() -> glib::ExitCode {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Akira")
+        .build();
+
+    // Present window
+    window.present();
 }
