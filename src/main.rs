1ea92e31c9c612076ee01b6ffe51d14873296c3b
Alecaddd <castellani.ale@gmail.com>
2024-09-16 22:50:09 -0700

    Build a simple application window with a granite headerbar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+/**
+ * This file is part of Akira.
+ *
+ * Copyright (c) 2024 Alessandro Castellani
+ *
+ * Akira is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as  published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Akira is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Akira. If not, see <https://www.gnu.org/ licenses/>.
+ */
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application};
+
+mod window;
+
+use self::window::AppWindow;
 
 const APP_ID: &str = "com.github.akiraux.akira";
 
@@ -16,10 +36,7 @@ fn main() -> glib::ExitCode {
 
 fn build_ui(app: &Application) {
     // Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("Akira")
-        .build();
+    let window = AppWindow::new(app);
 
     // Present window
     window.present();
---
+++ src/window.rs
@@ -0,0 +1,130 @@
+/**
+ * This file is part of Akira.
+ *
+ * Copyright (c) 2024 Alessandro Castellani
+ *
+ * Akira is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as  published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Akira is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Akira. If not, see <https://www.gnu.org/ licenses/>.
+ */
+use gettextrs::gettext;
+use gtk::subclass::prelude::*;
+use gtk::{gio, gio::Settings, glib, prelude::*, ApplicationWindow};
+use once_cell::sync::OnceCell;
+
+mod imp {
+    use super::*;
+
+    #[derive(Debug, Default)]
+    pub struct AppWindow {
+        pub settings: OnceCell<Settings>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for AppWindow {
+        const NAME: &'static str = "AppWindow";
+        type Type = super::AppWindow;
+        type ParentType = ApplicationWindow;
+    }
+
+    impl ObjectImpl for AppWindow {
+        fn constructed(&self) {
+            self.parent_constructed();
+
+            let obj = self.obj();
+
+            let mode_switch = granite::ModeSwitch::builder()
+                .primary_icon_name("display-brightness-symbolic")
+                .secondary_icon_name("weather-clear-night-symbolic")
+                .primary_icon_tooltip_text(gettext("Light Background"))
+                .secondary_icon_tooltip_text(gettext("Dark Background"))
+                .valign(gtk::Align::Center)
+                .build();
+
+            let gtk_settings = gtk::Settings::default().expect("Unable to get GtkSettings object");
+            mode_switch
+                .bind_property("active", &gtk_settings, "gtk-application-prefer-dark-theme")
+                .bidirectional()
+                .build();
+
+            let headerbar = gtk::HeaderBar::builder().show_title_buttons(true).build();
+
+            headerbar.style_context().add_class("default-decoration");
+            headerbar.pack_end(&mode_switch);
+
+            obj.set_titlebar(Some(&headerbar));
+        }
+    }
+
+    impl WidgetImpl for AppWindow {}
+    impl WindowImpl for AppWindow {
+        fn close_request(&self) -> glib::Propagation {
+            // self.obj()
+            //     .save_window_size()
+            //     .expect("Failed to save window state");
+
+            glib::Propagation::Proceed
+        }
+    }
+    impl ApplicationWindowImpl for AppWindow {}
+}
+
+glib::wrapper! {
+    pub struct AppWindow(ObjectSubclass<imp::AppWindow>)
+        @extends gtk::Widget, gtk::Window, gtk::ApplicationWindow,
+        @implements gio::ActionGroup, gio::ActionMap;
+}
+
+impl AppWindow {
+    pub fn new<P: IsA<gtk::Application>>(application: &P) -> Self {
+        glib::Object::builder()
+            .property("application", application)
+            .property("title", "Akira")
+            .build()
+    }
+
+    fn setup_settings(&self) {
+        let settings = Settings::new(super::APP_ID);
+        self.imp()
+            .settings
+            .set(settings)
+            .expect("`settings` should not be set before calling `setup_settings`.");
+    }
+
+    fn settings(&self) -> &Settings {
+        self.imp()
+            .settings
+            .get()
+            .expect("`settings` should be set in `setup_settings`.")
+    }
+
+    pub fn save_window_size(&self) -> Result<(), glib::BoolError> {
+        let size = self.default_size();
+
+        self.settings().set_int("window-width", size.0)?;
+        self.settings().set_int("window-height", size.1)?;
+        self.settings()
+            .set_boolean("is-maximized", self.is_maximized())?;
+
+        Ok(())
+    }
+
+    fn load_window_size(&self) {
+        let width = self.settings().int("window-width");
+        let height = self.settings().int("window-height");
+        let is_maximized = self.settings().boolean("is-maximized");
+
+        self.set_default_size(width, height);
+
+        if is_maximized {
+            self.maximize();
+        }
+    }
+}
