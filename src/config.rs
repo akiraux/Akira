53a8332d2539b03f3c672d1bd236eb2ab899a667
Alecaddd <castellani.ale@gmail.com>
2024-09-18 18:50:01 -0700

    Simple meson build

---
+++ src/config.rs
@@ -0,0 +1,5 @@
+pub static APP_ID: &str = "com.github.akiraux.akira";
+pub static SLASHED_APP_ID: &str = "/com/github/akiraux/akira/";
+pub static GETTEXT_PACKAGE: &str = "com.github.akiraux.akira";
+pub static LOCALEDIR: &str = "/usr/local/share/locale";
+pub static PKGDATADIR: &str = "/usr/local/share/com.github.akiraux.akira";
