6b22c58bc8167669bf4e7b2f9fccd4e8ad02ba0c
Alecaddd <castellani.ale@gmail.com>
2024-09-19 21:16:24 -0700

    Remove the auto generated config file

--- src/config.rs
+++
@@ -1,9 +0,0 @@
-pub static APP_ID: &str = "com.github.akiraux.akira";
-#[allow(dead_code)]
-pub static SLASHED_APP_ID: &str = "/com/github/akiraux/akira/";
-#[allow(dead_code)]
-pub static GETTEXT_PACKAGE: &str = "com.github.akiraux.akira";
-#[allow(dead_code)]
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-#[allow(dead_code)]
-pub static PKGDATADIR: &str = "/usr/local/share/com.github.akiraux.akira";
